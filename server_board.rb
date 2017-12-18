893a0768b1f624212fa3bbe691ff36b67db076a8
岡本勇太 <yuta_okamoto428@yahoo.co.jp>
2017-12-18 16:29:35 +0900

    不要なコード削除

--- server_board.rb
+++ server_board.rb
@@ -3,63 +3,33 @@ require "./base_board.rb"
 # 盤面情報を格納するクラス
 class ServerBoard < BaseBoard
 
-  # その場所に置くことが出来るか。
-  # x : 横
-  # y : 縦
-  # color : コマの色
-  # return is_none : 空きマス(none)か
-  def can_put?(x, y, color)
-    get_square(x, y) == FIELD[:none]
-  end
-
-  # ひっくり返せるか
-  # x : 横
-  # y : 縦
-  # color : コマの色
-  # return can_reverse : 周りにコマがあり、ひっくり返すことが出来る
-  def can_reverse?(x, y, color)
-    # チェック方向
-    #         上     右上      右      右下      下       左下       左       左上
-    step = [[0, 1], [1, 1], [1, 0], [1, -1], [0, -1], [-1, -1], [-1, 0], [-1, 1]]
-    # reverse_list = []
-    # step.each do |s|
-    #   cx, cy = x, y
-    #   loop do
-    #     list = []
-    #     cx += s[0]; cy += s[1]
-    #     break if cx >= 0 && cy >= 0 && cx < SQUARES && cy < SQUARES
-    #     square = get_square(cx, cy)
-    #     if square == COLOR[:none]
-    #   end
-    # end
-  end
-
   # コマを置く
+  # x : 横
+  # y : 縦
+  # color : コマの色
+  # return reverse_count : ひっくり返した数
   def put(x, y, color)
+    set_square(x, y, color)
+    stones = get_reverse_stones(x, y, color)
+    reverse_count = reverse(stones)
+    return reverse_count
+  end
+
+  private
+  # ひっくり返す石の座標リストをもらう
+  # x : 横
+  # y : 縦
+  # color : コマの色
+  # return ひっくり返す石の座標リスト
+  def get_reverse_stones(x, y, color)
+
 
   end
 
   # ひっくり返す
-  #
-  def reverse()
+  # stones : ひっくり返す石の座標リスト
+  # return ひっくり返した数
+  def reverse(stones)
 
-  end
-
-  # 記号文字で出力
-  # 改行コード入り文字列で渡すので受取側でputsして下さい
-  def pretty_print
-    header = "  #{(1..SQUARES).to_a.join(' ')}\n"   # 列番号
-    body = ""
-    @field.each_with_index do |line, i|
-      body += "#{i+1} "                             # 行番号
-      body += line                                  # 2次元配列の要素 => [0, 0, 1, -1, ...]
-        .map{|l| FIELD.key(l)}                      # 値に対応するキーを取得 => [:none, :none, :white, :black]
-        .map{|k| MARK[k]}                           # キーから出力文字取得 => ["□", "□", "○", "●"]
-        .join(' ') + "\n"                           # 文字列に連結 => "□ □ ○ ●"
-    end
-    header + body
   end
 end
-
-board = ServerBoard.new
-puts board.pretty_print
